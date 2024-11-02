451d74e7f59002e1547e50b93a8a2bf56024ef20
krishnaniyengar <60265798+krishnaniyengar@users.noreply.github.com>
2024-11-02 17:53:38 +0530

    programmatically updating json file on 11/2/2024, 5:53:38 PM by ui-daddy.

--- projects/external-components/src/public-api.ts
+++ projects/external-components/src/public-api.ts
@@ -17,3 +17,4 @@ export * from 'primeng/calendar';
 export {ToggleButtonModule, ToggleButton} from 'primeng/togglebutton';
 export * from './lib/primeng-selectors/util/util';
 export * from './lib/primeng-selectors/components/generate-with-ai/generate-with-ai.component';
+export * from './lib/primeng-selectors/components/test-component-external/test-component-external.component';
